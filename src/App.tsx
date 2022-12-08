4376d3aecf7751b4d0f80e17a3582ece7246ecae
vladislav <trololo557@gmail.com>
2022-12-08 16:47:12 +0300

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Component from 'components/Container'
 import './App.css';
 import TheHeader from 'components/TheHeader';
+import Search from 'components/Search';
 
 function App() {
   return (
     <Component>
       <TheHeader />
+      <Search hasError onSubmit={() => { }} />
     </Component>
   );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+
+import { ReactComponent as SearchIcon } from 'assets/icon-search.svg';
+import styles from './Search.module.scss';
+import Button from 'components/Button';
+
+interface SearchProps {
+  hasError: boolean,
+  onSubmit: (text: string) => void,
+}
+
+type FormFields = {
+  username: HTMLInputElement;
+}
+
+const Search: FC<SearchProps> = ({ hasError, onSubmit }: SearchProps) => {
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields>) => {
+    event.preventDefault();
+    const text = event.currentTarget.username.value;
+    if (text) {
+      onSubmit(text);
+      event.currentTarget.reset();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete='off'>
+      <div className={styles.Search}>
+        <label htmlFor='search' className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          type='text'
+          className={styles.textField}
+          id='search'
+          name='username'
+          placeholder='Search GitHub username...'
+        />
+        {hasError && (
+          <div className={styles.error}>No result</div>
+        )}
+        <Button>
+          Search
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
