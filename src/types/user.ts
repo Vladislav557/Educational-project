cd811db520fcbd904dda714fc1c443613c8838d3
vladislav <trololo557@gmail.com>
2022-12-08 12:03:48 +0300

    prepare project

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+export type GithubUser = {
+    login: string;
+    id: number;
+    avatar_url: string;
+    name: string;
+    company: string;
+    block: string;
+    location: string;
+    bio: string;
+    twitter_username: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    create_at: string;
+}
