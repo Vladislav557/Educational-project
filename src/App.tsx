1ea1497a0dc2bfb89cf75931e54a21d0f39b738f
vladislav <trololo557@gmail.com>
2022-12-08 15:13:49 +0300

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Component from 'components/Container'
 import './App.css';
+import TheHeader from 'components/TheHeader';
 
 function App() {
   return (
     <Component>
-
+      <TheHeader />
     </Component>
   );
 }
--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
+
+import ThemeSwitcher from 'components/ThemeSwitcher';
 import styles from './TheHeader.module.scss';
 
-interface TheHeaderProps {}
-
-const TheHeader: FC<TheHeaderProps> = () => (
+const TheHeader: FC = () => (
   <div className={styles.TheHeader}>
-    TheHeader Component
+    <div className={styles.logo}>
+      devfinder
+    </div>
+    <ThemeSwitcher />
   </div>
 );
 
